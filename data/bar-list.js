24af7f9b5d12b9f6d162357afbfae92204726870
Joseph Tatum <josephtatum@gmail.com>
2019-10-21 14:23:15 -0700

    added establishments for SOutheast and Downtown to bar-list.js fil

---
+++ data/bar-list.js
@@ -0,0 +1,307 @@
+const aaltoLoungeBar = {
+    id: 'aalto-lounge',
+    name: 'Aalto Lounge',
+    quadrant: 'Southeast',
+    district: 'Belmont',
+    address: '3365 SE Belmont St',
+    dateNight: true,
+    beer: '$9 Beer Buckets (Four Bottles)',
+    food: '$3 Appetizer Special',
+    liquor: '$3 House Cocktail, $4 Wells',
+    time: '5:00 PM - 7:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const sweetHereafterBar = {
+    id: 'sweet-hereafter',
+    name: 'Sweet Hereafter',
+    quadrant: 'Southeast',
+    district: 'Belmont',
+    address: '3326 SE Belmont St',
+    dateNight: true,
+    beer: '$1 Off Draft Beer (excludes Rainier)',
+    food: '$3 Chips and Salsa',
+    liquor: '$1 Off Wells, $1 Off House Wines',
+    time: '4:00 PM - 7:00 PM',
+    days: 'Mon - Fri',
+    favorite: false,
+};
+
+const tripleNickelBar = {
+    id: 'triple-nickel',
+    name: 'Triple Nickel',
+    quadrant: 'Southeast',
+    district: 'Belmont',
+    address: '3646 SE Belmont St',
+    dateNight: false,
+    beer: '$2 PBR',
+    food: '"Food from $5 - $8"',
+    liquor: '$3 Wells, $1 Off House Wines',
+    time: '11:00 AM - 7:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const theLiquorStoreBar = {
+    id: 'the-liquor-store',
+    name: 'The Liquor Store',
+    quadrant: 'Southeast',
+    district: 'Belmont',
+    address: '3341 SE Belmont St',
+    dateNight: false,
+    beer: '$2 Mug of Beer',
+    food: '$2 Tacos',
+    liquor: '$1 Off All Cocktails',
+    time: '4:00 PM - 7:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const conquistadorLoungeBar = {
+    id: 'conquistador-lounge',
+    name: 'Conquistador Lounge',
+    quadrant: 'Southeast',
+    district: 'Belmont',
+    address: '2045 SE Belmont St',
+    dateNight: true,
+    beer: '$2 Rainier Pints',
+    food: '"Food Specials"',
+    liquor: '$5.50 Margaritas',
+    time: '4:00 PM - 7:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const goldDustMeridianBar = {
+    id: 'gold-dust-meridian',
+    name: 'Gold Dust Meridian',
+    quadrant: 'Southeast',
+    district: 'Hawthorne',
+    address: '3267 SE Hawthorne Blvd',
+    dateNight: true,
+    beer: '$1 Off Pints',
+    food: '$6 Mac and Cheese, $5 Deviled Eggs',
+    liquor: '$1 Off Wells',
+    time: '2:00 PM - 8:00 PM',
+    days: 'Monday - Saturday (All Day Sunday)',
+    favorite: false,
+};
+
+const backStageBar = {
+    id: 'back-stage-bar',
+    name: 'Back Stage Bar',
+    quadrant: 'Southeast',
+    district: 'Hawthorne',
+    address: '3702 SE Hawthorne Blvd',
+    dateNight: true,
+    beer: '$1 Off McMenamins Ales',
+    food: '$4.50 Truffle Fries, $4 6 inch Pepperoni Pizza',
+    liquor: '$4.75 Wells',
+    time: '3:00 PM - 6:00 PM & 10:00 PM - Midnight',
+    days: 'Daily',
+    favorite: false,
+};
+
+const toffeeClubBar = {
+    id: 'toffee-club',
+    name: 'Toffee Club',
+    quadrant: 'Southeast',
+    district: 'Hawthorne',
+    address: '1006 SE Hawthorne Blvd',
+    dateNight: false,
+    beer: '$2.50 Tecate & Rainier Cans',
+    food: 'Food from $4 - $6',
+    liquor: '$6 Dark & Stormy or Moscow Mule',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Tuesday - Friday (All Day Monday)',
+    favorite: false,
+};
+
+const hawthorneHideawayBar = {
+    id: 'hawthorne-hideaway',
+    name: 'Hawthorne Hideaway',
+    quadrant: 'Southeast',
+    district: 'Hawthorne',
+    address: '2221 SE Hawthorne Blvd',
+    dateNight: false,
+    beer: '$2.50 PBR',
+    food: 'N/A',
+    liquor: '$3 Wells',
+    time: '3:00 PM - 7:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const neatBar = {
+    id: 'neat',
+    name: 'Neat',
+    quadrant: 'Southeast',
+    district: 'Hawthorne',
+    address: '2637 SE Hawthorne Blvd',
+    dateNight: true,
+    beer: '$1 off Beer',
+    food: '$4 Potato Special',
+    liquor: '$1 Off Wells & Cocktails',
+    time: '4:00 PM - 6:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const olympiaProvisionsBar = {
+    id: 'olympia-provisions',
+    name: 'Olympia Provisions Public House Eatery',
+    quadrant: 'Southeast',
+    district: 'Division',
+    address: '3384 SE Division St',
+    dateNight: true,
+    food: '$5 Classic Cheesburger & $5 Bratwurst',
+    time: '3:00 PM - 6:00 PM & 10:00 PM - close on Friday and Saturday',
+    days: 'Monday - Saturday',
+    favorite: false,
+};
+
+const doubleBarrelBar = {
+    id: 'double-barrel',
+    name: 'Double Barrel',
+    quadrant: 'Southeast',
+    district: 'Division',
+    address: '2002 SE Division St',
+    dateNight: false,
+    beer: '$1 Off Drafts',
+    food: '$6 Happy Hour Burger',
+    liquor: '$1 Off Wells',
+    time: '2:00 PM - 7:00 PM',
+    days: 'Monday - Friday (All Day Sunday)',
+    favorite: false,
+};
+
+const northBar = {
+    id: 'north-bar',
+    name: 'North Bar',
+    quadrant: 'Southeast',
+    district: 'Division',
+    address: '5008 SE Division St',
+    dateNight: true,
+    beer: '$2 Hamms, $4 Micros',
+    food: '$$1 Mixed Nuts, $3 Cheese Quesadilla',
+    liquor: '$3 Well Drinks',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const automaticBar = {
+    id: 'automatic-bar',
+    name: 'Automatic Bar',
+    quadrant: 'Southeast',
+    district: 'Division',
+    address: '3652 SE Division St',
+    dateNight: true,
+    beer: '$1 Off Micros',
+    food: '$1 Off All Food',
+    liquor: '$5 House Cocktails',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const bambooSushiBar = {
+    id: 'bamboo-sushi',
+    name: 'Bamboo Sushi',
+    quadrant: 'Downtown',
+    district: 'Downtown',
+    address: '404 SW 12th Ave',
+    dateNight: true,
+    beer: '$4 Sapporo',
+    food: '$3 Chili Lime Edamame, $9 Green Machine Roll',
+    liquor: '$8 Toki Highball Cocktail',
+    time: 'Opening - 6:00 PM',
+    days: 'Weekdays',
+    favorite: false,
+};
+
+const kellsIrishPubBar = {
+    id: 'kells-irish-bar',
+    name: 'Kells Irish Pub',
+    quadrant: 'Downtown',
+    district: 'Downtown',
+    address: '112 SW 2nd Ave',
+    dateNight: false,
+    beer: '$4 off all Kells Beers',
+    food: '$4 Kells Fries, $7 Kells Whiskey Wings',
+    liquor: '$5 Cocktail of the Week',
+    time: '3:00 PM - 6:00 PM (Sunday: 7:00 PM to close)',
+    days: 'Monday - Friday, Sunday',
+    favorite: false,
+};
+
+const punchBowlSocialBar = {
+    id: 'punch-bowl-social',
+    name: 'Kells Irish Pub',
+    quadrant: 'Downtown',
+    district: 'Downtown',
+    address: '340 SW Morrison St',
+    dateNight: true,
+    beer: '$3 PBR, $4 Hamms',
+    food: '$2.50 Roaasted Chicken Taco, $7 Spiced Edamame',
+    liquor: '$6 Wells',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const barlowPDXBar = {
+    id: 'barlow',
+    name: 'Barlow',
+    quadrant: 'Downtown',
+    district: 'Downtown',
+    address: '',
+    dateNight: true,
+    beer: '$1 Off Draft Beer',
+    food: '$5 Shoestring Fries, $ Artisan Grilled Cheese',
+    liquor: '$7 Cocktails',
+    time: '4:00 PM - 6:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const virginiaCafeBar = {
+    id: 'virginia-bar',
+    name: 'Virginia Bar',
+    quadrant: 'Downtown',
+    district: 'Downtown',
+    address: '820 SW 10th Ave',
+    dateNight: true,
+    beer: '$1 Off Drafts, $5.75 Boneyard IPA',
+    food: '$4 Chips and Salsa, $6 Chili CHeese Fries',
+    liquor: '$10 Cocktails',
+    time: '4:00 PM - 7:00 PM',
+    days: 'Daily',
+    favorite: false,
+};
+
+const listOfBars = [
+    aaltoLoungeBar,
+    sweetHereafterBar,
+    tripleNickelBar,
+    theLiquorStoreBar,
+    conquistadorLoungeBar,
+    goldDustMeridianBar,
+    backStageBar,
+    toffeeClubBar,
+    hawthorneHideawayBar,
+    neatBar,
+    olympiaProvisionsBar,
+    doubleBarrelBar,
+    northBar,
+    automaticBar,
+    bambooSushiBar,
+    kellsIrishPubBar,
+    punchBowlSocialBar,
+    barlowPDXBar,
+    virginiaCafeBar,
+
+];
+
+export default listOfBars;
