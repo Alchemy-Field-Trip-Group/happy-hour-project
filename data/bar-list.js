c345fa99cef56d0ea67775a6c7c4c72dfc5b9437
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-10-21 14:27:53 -0700

    added mississippi and st johns objects to data/bar-list.js

---
+++ data/bar-list.js
@@ -0,0 +1,119 @@
+const theRamblerBar = {
+    id: 'the-rambler',
+    name: 'The Rambler',
+    quadrant: 'North',
+    district: 'Mississippi',
+    address: '4205 N Mississippi Ave',
+    dateNight: false,
+    beer: '$1 off all beers on tap',
+    food: '$4 jojos, $7 poutine',
+    liquor: '$1 off all signature cocktails and well spirits',
+    time: '4:00 PM - 6:00 PM',
+    days: 'Tuesday - Friday (All day Monday)',
+    favorite: false,
+};
+
+const interurbanBar = {
+    id: 'the-rambler',
+    name: 'The Rambler',
+    quadrant: 'North',
+    district: 'Mississippi',
+    address: '4057 N Mississippi Ave',
+    dateNight: true,
+    beer: '$4 20oz pours',
+    food: '$3 olives & almonds, $9 pate board',
+    liquor: '$5 signature cocktails',
+    time: '4:00 PM - 6:00 PM',
+    days: 'Monday - Friday',
+    favorite: false,
+};
+
+
+const porQueNoBar = {
+    id: 'por-que-no',
+    name: '¿Por Qué No?',
+    quadrant: 'North',
+    district: 'Mississippi',
+    address: '3524 N Mississippi Ave',
+    dateNight: true,
+    beer: '$.50 off beer & cider',
+    food: '$.50 off tacos, $1 off chips & dip, and bowls',
+    liquor: '$1.50 off margaritas',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Monday - Sunday (all day Tuesday)',
+    favorite: false,
+};
+
+const mississippiPizzaBar = {
+    id: 'mississippi-pizza',
+    name: 'Mississippi Pizza',
+    quadrant: 'North',
+    district: 'Mississippi',
+    address: '3552 N Mississippi Ave',
+    dateNight: false,
+    beer: '$.50 off pints, $1 off pitchers',
+    food: '$3 cheese, pepperoni, and vegan slices',
+    time: '3:00 PM - 5:00 PM',
+    days: 'Sunday - Wednesday',
+    favorite: false,
+};
+
+const libertyGlassBar = {
+    id: 'liberty-glass',
+    name: 'Liberty Glass',
+    quadrant: 'North',
+    district: 'Mississippi',
+    address: '3552 N Mississippi Ave',
+    dateNight: true,
+    beer: '$1.50 miller high life stubbie',
+    food: '$8 bacon cheese fries, $9 liberty burger',
+    time: '3:00 PM - 7:00 PM',
+    days: 'Everyday',
+    favorite: false,
+};
+
+const overEasyBar = {
+    id: 'over-easy-bar',
+    name: 'Over Easy Bar',
+    quadrant: 'North',
+    district: 'St Johns',
+    address: '6801 Columbia Way',
+    dateNight: true,
+    beer: '$3.50 microbrews, $2.25 domestics',
+    food: '$5 burgers, $6 nachos',
+    liquor: '$3 single mix - one shot',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Everyday ($3 micros on Tuesday)',
+    favorite: false,
+};
+
+const stJohnsPubBar = {
+    id: 'st-johns-pub',
+    name: 'St Johns Pub (McMenamins)',
+    quadrant: 'North',
+    district: 'St Johns',
+    address: '8203 N Ivanhoe St',
+    dateNight: true,
+    beer: '$1 off pints',
+    food: '$5 burgers, $6 nachos',
+    liquor: '$3.50 fries, $6.25 BBQ pork sliders',
+    time: '3:00 PM - 6:00 PM, 10:00pm - 12:00am',
+    days: 'Everyday',
+    favorite: false,
+};
+
+const theRangerTavern = {
+    id: 'the-ranger-tavern',
+    name: 'The Ranger Tavern',
+    quadrant: 'North',
+    district: 'St Johns',
+    address: '9520 N Lombard',
+    dateNight: false,
+    beer: '$2 PBR tallboys',
+    time: '3:00 PM - 6:00 PM',
+    days: 'Monday-Friday',
+    favorite: false,
+};
+
+export const listOfBars = [theRamblerBar, interurbanBar, porQueNoBar, mississippiPizzaBar, libertyGlassBar, overEasyBar, stJohnsPubBar, theRangerTavern]; 
+
