0339dedc66d29d09f161148d6231715fdd8dff4d
Joseph Tatum <josephtatum@gmail.com>
2019-10-23 16:33:03 -0700

    created loop for generating district specific bars

--- results/results.js
+++ results/results.js
@@ -4,11 +4,13 @@ import { findById } from '../common/utils.js';
 
 let userPreferences = JSON.parse(localStorage.getItem('preference'));
 
+const searchParam = new URLSearchParams(window.location.search);
+const districtId = searchParam.get('id');
 
 // let userDistrict = JSON.parse(localStorage.getItem('district'));  
 
 let userPreferenceFilteredArray = []; 
-// let chosenDistrictArray = []; 
+let arrayToDisplay = []; 
 
 
 for (let i = 0; i < userPreferences.length; i++) {	//  
@@ -24,6 +26,17 @@ for (let i = 0; i < userPreferences.length; i++) {	//
     });
 }
 
+console.log(userPreferenceFilteredArray);
+
+userPreferenceFilteredArray.forEach(bar => {
+    if(bar.district === districtId) {
+        arrayToDisplay.push(bar);
+    }
+});
+
+console.log(arrayToDisplay)
+
+
 
 let favoritesArray = localStorage.getItem('favorites');
 
@@ -36,7 +49,7 @@ if (favoritesArray === null) {
 
 const resultsUl = document.getElementById('results-list');
 
-userPreferenceFilteredArray.forEach(bar => {
+arrayToDisplay.forEach(bar => {
 
     let thisBar = bar;
 
