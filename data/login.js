12b03da05c5a635cc0cbfd041474d32ae36e1836
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-10-23 17:49:45 -0700

    added some deleted files

--- data/login.js
+++ data/login.js
@@ -3,21 +3,26 @@ import { makeUser } from '/common/utils.js';
 
 const form = document.querySelector('form');
 const logInButton = document.getElementById('login-button');
+const userName = document.getElementById('username');
+const password = document.getElementById('password');
 
 logInButton.addEventListener('click', (event) => {
     event.preventDefault();
-
-    const formData = new FormData(form);
-
-    const createdUser = makeUser(formData);
-
-    console.log(createdUser);
-
-    JSON.stringify(localStorage.setItem('username', createdUser.username));
-
-    JSON.stringify(localStorage.setItem('password', createdUser.password));
+    if(password.value === '' || userName.value === ''){
+        return;
+    } else {
+        const formData = new FormData(form);
     
-    window.location = '/preferences/preferences.html';
+        const createdUser = makeUser(formData);
+    
+        console.log(createdUser);
+    
+        JSON.stringify(localStorage.setItem('username', createdUser.username));
+    
+        JSON.stringify(localStorage.setItem('password', createdUser.password));
+        
+        window.location = '/preferences/preferences.html';
+    }
 });
 
 
